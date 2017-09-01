c263ff2a81d34af5006c8cdd824a270c9f0086c0
Jwrighty <jason.wrighty@gmail.com>
2017-09-01 12:29:37 +0100

    added jwt authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  scope :api do
+   resources :users, except: [:create]
+   post 'register', to: 'authentications#register'
+   post 'login', to: 'authentications#login'
+ end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
