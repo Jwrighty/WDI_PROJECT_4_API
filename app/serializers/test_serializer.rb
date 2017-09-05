3e6f004f0505190ac34eb175e860c6dc166c5643
Jwrighty <jason.wrighty@gmail.com>
2017-09-05 18:49:30 +0100

    images added to tests and seeds updated

--- app/serializers/test_serializer.rb
+++ app/serializers/test_serializer.rb
@@ -1,5 +1,5 @@
 class TestSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :image
   has_many :characters
   has_many :words
   has_many :scores
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904142407) do
+ActiveRecord::Schema.define(version: 20170905125845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170904142407) do
     t.string "name"
     t.integer "character_id"
     t.integer "word_id"
+    t.string "image"
   end
 
   create_table "tests_words", id: false, force: :cascade do |t|
